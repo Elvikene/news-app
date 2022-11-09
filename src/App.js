c87645d47ae39fe7aa90d55d0ae8acc83e416ade
Elvikene <elvikene@gmail.com>
2022-11-09 16:57:33 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import NewsGroupComponent from './Body';
+import HeaderComponent from './Header';
+import PaginationComponent from './Footer';
+
+
+function App() {
+  return (
+<Container>
+  <HeaderComponent/>
+  <NewsGroupComponent />
+  <PaginationComponent />
+</Container>
+  );
+}
+
+export default App;
