2a792a5acbf3d09ded32e24fb0367e1a1ffec3e3
Elvikene <elvikene@gmail.com>
2022-11-14 13:01:25 +0200

    Set page limit

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,17 +1,27 @@
 import { useState } from 'react';
 import Pagination from 'react-bootstrap/Pagination';
 
+
+let paginationConfig = {
+  totalPages: 30,
+  currentPage: 15,
+  size: "lg",
+  prevNext: true, 
+ };
+
 function PaginatonComponent() {
-
   const[page, setPage] = useState(1);
-
+ 
+  
   return (
-    <Pagination className="mt-4 justify-content-end">
-      <Pagination.Prev onClick={( )=> setPage(page -1)} />
-      <Pagination.Item disabled>{page} / 140</Pagination.Item>
-      <Pagination.Next onClick={( )=> setPage(page +1)}  />
+    <Pagination className="mt-4 justify-content-end" {...paginationConfig}>
+      <Pagination.Prev  onClick={( )=> setPage(page -1)} disabled={page<=1 && true }  />
+      <Pagination.Item disabled>{page} / 30</Pagination.Item>
+      <Pagination.Next onClick={( )=> setPage(page +1)} disabled={page >= 30 && true }  />
     </Pagination>
   );
 }
+
+
 
 export default PaginatonComponent;
